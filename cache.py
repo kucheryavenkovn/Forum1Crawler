b28aea41245d6ef24e3ff73b2541f94ed89f60d9
Арипов Никита <aripovn@gmail.com>
2020-12-19 18:45:44 +0300

    Добавил сохранение кэша для монго

--- cache.py
+++ cache.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
+import pymongo
 from json import dump, load
-
 from settings import data_directory
 from settings import save_to
-from mongo_io_driver import last_id_mongo
+from secret import mongo_db_data
 
 
 class Cache:
@@ -18,23 +18,32 @@ class Cache:
         Восстанавливет ID последней отработанной темы.
         :return: None
         """
-        try:
-            if save_to() == 'file':
+        if save_to() == 'file':
+            try:
                 data = open(data_directory() + 'last_id.data')
-            else:
-                # TODO Add mongo cache
-                data = last_id_mongo()
-        except IOError:
-            print('Внимание! Не найден файл ID темы. Будет создан новый.')
-            return 0
+            except IOError:
+                print('Внимание! Не найден файл ID темы. Будет создан новый.')
+                return 0
         else:
-            with data:
-                return load(data)
+            mongo_data = mongo_db_data()
+            client = pymongo.MongoClient(mongo_data.ip, mongo_data.port)
+            db = client[mongo_data.db]
+            data = db.cache.find_one()
+            if data is None:
+                print('Внимание! Не найдена запись ID темы. Будет записана новая.')
+
+        return load(data)
 
     def save(self):
         """
-        Сохраняет кеш на диск. При повторном запуске краулера кеш восстанавливается в load()
+        Сохраняет кеш. При повторном запуске краулера кеш восстанавливается в load()
         :return: None
         """
-        with open(data_directory() + 'last_id.data', 'w') as data:
-            dump(self.last_id, data)
+        if save_to() == 'file':
+            with open(data_directory() + 'last_id.data', 'w') as data:
+                dump(self.last_id, data)
+        else:
+            mongo_data = mongo_db_data()
+            client = pymongo.MongoClient(mongo_data.ip, mongo_data.port)
+            db = client[mongo_data.db]
+            db.cache.update_one({}, self.last_id)
