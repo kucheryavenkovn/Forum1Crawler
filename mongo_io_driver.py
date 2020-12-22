b9fc36ed5500057116c19e93cd4f23c3bdefa57c
Никита Арипов <arin@1c.ru>
2020-12-22 17:44:41 +0300

    Добавил еще данных для статистики

--- mongo_io_driver.py
+++ mongo_io_driver.py
@@ -3,7 +3,7 @@
 import pymongo
 import datetime
 from secret import mongo_db_data
-from settings import mongo_db, mongo_table
+from settings import mongo_db, mongo_table, base_url
 
 
 class MongoIODriver:
@@ -11,11 +11,13 @@ class MongoIODriver:
     Запись в MongoDB с полями:
         * datetime - время и дата сообщения,
         * category - раздел форума,
+        * topic - тема форума,
         * total_score - итоговый рейтинг сообщения: положительный рейтинг + отрицательный рейтинг,
         * pros_score - положительный рейтинг,
         * cons_score - отрицательный рейтинг,
         * id - идентификатор сообщения,
-        * url - ссылка на сообщение,
+        * url - идентификатор темы,
+        * full_url - идентификатор темы,
         * username - имя пользователя,
         * company - компания в которой работает пользователь,
         * text - текст сообщения.
@@ -29,9 +31,14 @@ class MongoIODriver:
     def save_messages(self, parser):
         for message in parser.messages:
             message_dict = message.message_representation()
-
-            message_dict['category'] = message_dict['category'].strip()
-            # Приведем дату к типу
+            # Разделим заголовок на раздел и тему
+            category = message_dict['category'].strip().split('→')
+            message_dict['category'] = category[0].strip()
+            message_dict['topic'] = category[1].strip()
+            # Приведем строковое представление даты к типу datetime
             message_dict['datetime'] = datetime.datetime.strptime(message_dict['datetime'], '%d.%m.%Y %H:%M')
+            # Соберем полный адрес сообщения
+            message_dict['full_url'] = '{base_url}/t/{topic_url}/m/{message_url}'.format(
+                base_url=base_url(), topic_url=message_dict['url'], message_url=message_dict['id'])
 
             self.table.insert_one(message_dict)
